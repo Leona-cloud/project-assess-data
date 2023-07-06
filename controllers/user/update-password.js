5ef5ad7ad1c22c9962bd5e819c70932834066c21
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2023-07-06 18:21:11 +0100

    chore: create endpoint to update password (#4)

---
+++ controllers/user/update-password.js
@@ -0,0 +1,63 @@
+const User = require('../../models/User');
+const _ = require('lodash');
+const bcrypt = require('bcrypt')
+
+
+
+const updatePassword = async(req, res)=>{
+
+    const {email, token, password, confirmPassword} = req.body;
+
+    const userExists = await User.findOne({email: email, verificationToken: token});
+    if(!userExists) return res.status(400).json({
+        success: false,
+        message: 'Token invalid'
+    });
+
+    //check if passwords match
+    if(userExists.verificationTokenTTL <= Date.now()){
+        return res.status(400).json({
+            success: false,
+            message: `OTP provided expired for, ${userExists.email}`
+          })
+    };
+    
+    try {
+
+        if(_.isEqual(password, confirmPassword)){
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+
+            await userExists.updateOne({id: userExists.id}).set({
+                verificationToken: '',
+                verificationTokenTTL: '',
+                password: hash
+            })
+
+        }else{
+            return res.status(400).json({
+                success: false,
+                message: 'Passwords do not match'
+            })
+        };
+
+        return res.status(200).json({
+            success: true,
+            message: 'Password updated successfully'
+        });
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong - update password'
+        })
+    }
+
+
+};
+
+
+
+
+module.exports = updatePassword
--- routes/authentication.js
+++ routes/authentication.js
@@ -4,14 +4,18 @@ const auth = require('../middlewares/auth')
 
 const registerUser = require('../controllers/user/register');
 const userLogin = require('../controllers/user/login');
-const verifyMail = require('../controllers/user/verify-email')
+const verifyMail = require('../controllers/user/verify-email');
+const resetPassword = require('../controllers/user/reset-password');
+const updatePassword = require('../controllers/user/update-password')
 
 
 
 //user registration
 router.post('/register', registerUser);
 router.post('/login', userLogin);
-router.post('/verify-email', verifyMail)
+router.post('/verify-email', verifyMail);
+router.post('/reset-password', resetPassword);
+router.post('/update-password', updatePassword)
 
 
 
